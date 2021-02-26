51ec57275420c67753c6f5223dda66c5435a614e
tahmidmahdi <tahmid5550@gmail.com>
2021-02-26 22:25:59 +0600

    site is modified

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import './Shop.css'
+
+
+const Shop = () => {
+
+    const first10 = fakeData.slice(0, 10)
+    const [products, setProducts] = useState(first10)
+    return (
+        <div className='shop-container'>
+            <div className="product-container">
+                {
+                    products.map(product => <li>{product.name}</li>)
+                }
+            </div>
+            <div className="cart-container">
+                <h3>This is Cart</h3>
+            </div>
+
+
+
+
+        </div>
+    );
+};
+
+export default Shop;
