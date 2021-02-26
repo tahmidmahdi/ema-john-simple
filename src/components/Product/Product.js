150774d976de68af3f2478652dc5540c7f33e633
tahmidmahdi <tahmid5550@gmail.com>
2021-02-27 01:03:01 +0600

    cart is added but not fully functional

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 const Product = (props) => {
-    console.log(props.product.name);
+    // console.log(props);
     const { img, name, seller, price, stock } = props.product
     // console.log(name);
     return (
@@ -17,6 +19,10 @@ const Product = (props) => {
                 <p><small>by: {seller}</small></p>
                 <p>${price}</p>
                 <p><small>Only {stock} left in stock- Order Soon!</small></p>
+                <button className='main-button' onClick={() =>{
+                    props.handleAddProduct(props.product)
+                }}> <FontAwesomeIcon icon={faShoppingCart}/>
+                {' '} add to cart </button>
             </div>
         </div>
     );
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,16 +8,24 @@ const Shop = () => {
 
     const first10 = fakeData.slice(0, 10)
     const [products, setProducts] = useState(first10)
+    const [cart, setCart] = useState([]);
+    const handleAddProduct =(product) =>{
+        console.log('Product Added', product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     return (
         <div className='shop-container'>
             <div className="product-container">
                 {
                     //useState products  this is component name Product
-                    products.map(pd => <Product product={pd}></Product>)
+                    products.map(pd => <Product product={pd} handleAddProduct={handleAddProduct}>
+                    </Product>)
                 }
             </div>
             <div className="cart-container">
                 <h3>This is Cart</h3>
+                <h5>Order Summary: {cart.length}</h5>
             </div>
 
 
